3c5e6d9165c0fd8d652fb37e3c39402b5f4b005a
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-16 11:23:44 -0700

    [add] see list of providers on homepage

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+#Mock array of providers
+providers = [
+    {'title': 'DC', 'name': 'Lafferty'},
+    {'title': 'DDS', 'name': 'Rones'}
+]
+
 @app.route('/')
-def index():
-    """Returns homepage"""
-    return render_template ('home.html', msg='Health map!')
+def providers_index():
+    """Show all providers"""
+    return render_template ('providers_index.html', providers=providers)
 
 if __name__ == '__main__':
     app.run(debug=True)
