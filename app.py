120193bbbbea44829e21b13c14b309d7e92878c6
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-16 11:12:37 -0700

    [add] home and base templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     """Returns homepage"""
-    return 'Hello world!'
+    return render_template ('home.html', msg='Health map!')
 
 if __name__ == '__main__':
     app.run(debug=True)
