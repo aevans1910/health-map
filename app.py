1a2210b7b32f167f3db5b791a7655e8fef4469db
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-16 11:28:07 -0700

    [add] MongoDB and Providers model

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Health_Map
+providers = db.providers
 
 app=Flask(__name__)
 
 #Mock array of providers
-providers = [
-    {'title': 'DC', 'name': 'Lafferty'},
-    {'title': 'DDS', 'name': 'Rones'}
-]
+# providers = [
+#     {'title': 'DC', 'name': 'Lafferty'},
+#     {'title': 'DDS', 'name': 'Rones'}
+# ]
 
 @app.route('/')
 def providers_index():
     """Show all providers"""
-    return render_template ('providers_index.html', providers=providers)
+    return render_template ('providers_index.html', providers=providers.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
