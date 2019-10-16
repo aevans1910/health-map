4a0f10abefd8d48328d454c3b0a2607370abcf33
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-16 12:14:25 -0700

    [add] new provider profiles

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -17,6 +17,23 @@ app=Flask(__name__)
 def providers_index():
     """Show all providers"""
     return render_template ('providers_index.html', providers=providers.find())
+
+@app.route('/providers/new')
+def providers_new():
+    """List a new provider"""
+    return render_template('providers_new.html')
+
+@app.route('/providers', methods=['POST'])
+def providers_submit():
+    """Submit a new provider profile"""
+    provider = {
+        'title' : request.form.get('title'),
+        'name' : request.form.get('name'),
+        'location' : request.form.get('location'),
+        'phone' : request.form.get('phone')
+    }
+    providers.insert_one(provider)
+    return redirect(url_for('providers_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
