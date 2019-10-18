f0dfd87043ae5108f09a9e8751b863fd8250ce49
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-17 22:10:34 -0700

    [fix] bugs

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient()
 db = client.Health_Map
 providers = db.providers
