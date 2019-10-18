b9809fa3bc77b7e4da9dfc198cfd94e03a73112d
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-17 22:19:08 -0700

    [fix] heroku bugs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient()
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.Health_Map
 providers = db.providers
 
@@ -71,5 +71,4 @@ def providers_delete(provider_id):
     return redirect(url_for('providers_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
